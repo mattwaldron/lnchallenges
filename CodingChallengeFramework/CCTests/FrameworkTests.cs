b3bc346b875a2b7dd8c5a94165bac8e3198f24c6
Matt Waldron <waldron.matthew@gmail.com>
2019-02-01 13:57:14 -0600

    adding unit tests fixing bug in maze generation

---
+++ CodingChallengeFramework/CCTests/FrameworkTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallengeFramework;
+
+namespace CCTests
+{
+    [TestClass]
+    public class FrameworkTests
+    {
+        [TestMethod]
+        public void Maze()
+        {
+            var m = MazeGenerator.GetMaze(7, 11);
+            foreach (var row in m)
+            {
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
---
+++ CodingChallengeFramework/CCTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CCTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cyberonics")]
+[assembly: AssemblyProduct("CCTests")]
+[assembly: AssemblyCopyright("Copyright © Cyberonics 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a39119f6-7554-47de-95f0-65ac8a1cefc2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
