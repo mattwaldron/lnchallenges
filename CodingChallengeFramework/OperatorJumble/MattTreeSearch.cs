456fba0c0817a6019b7b0feac9b0d6340de66a66
matt <waldron.matthew@gmail.com>
2019-06-10 21:35:49 -0500

    stripped down operator jumble tree search code and files

--- CodingChallengeFramework/OperatorJumble/MattTreeSearch.cs
+++ CodingChallengeFramework/OperatorJumble/MattTreeSearch.cs
@@ -1,90 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CodingChallengeFramework;
-
 using LookupDict = System.Collections.Concurrent.ConcurrentDictionary<(int val, int min, int max), string>;
-
+using LookupIndex = System.Collections.Generic.Dictionary<int, System.Collections.Generic.List<(int val, int min, int max)>>;
 
 namespace OperatorJumble
 {
     public partial class MattTreeSearch : IOperatorJumble
     {
-        public enum Op
+        /// <summary>
+        /// Initialize a LookupIndex with empty Lists for each expression length (1 to 9)
+        /// </summary>
+        LookupIndex GenerateIndex()
         {
-            M,
-            D,
-            A,
-            S,
-            E,
-            C
+            var index = new LookupIndex();
+            foreach (var v in Enumerable.Range(1, 9))
+            {
+                index[v] = new List<(int val, int min, int max)>();
+            }
+            return index;
         }
 
-        public static Dictionary<Op, Func<int, int, int>> evalDict = new Dictionary<Op, Func<int, int, int>>()
-        {
-            {Op.M, (a, b) => checked(a * b)},
-            {Op.D, (a, b) => {
-                if (a % b != 0) { return Int32.MaxValue; }
-                else { return a / b; }
-                } },
-            {Op.A, (a, b) => a + b},
-            {Op.S, (a, b) => a - b},
-            {Op.E, (a, b) => {
-                if (b < 0) { return Int32.MaxValue; }
-                else {return checked((Int32) Math.Pow((double) a, (double) b)); }
-                } },
-            {Op.C, (a, b) => (a >= 0 && b >= 0) ? Convert.ToInt32($"{a}{b}") : Int32.MaxValue}
-        };
-
-        public static Dictionary<Op, Func<string, string, string>> exprDict = new Dictionary<Op, Func<string, string, string>>()
-        {
-            {Op.M, (a, b) => $"({a} * {b})"},
-            {Op.D, (a, b) => $"({a} / {b})"},
-            {Op.A, (a, b) => $"({a} + {b})"},
-            {Op.S, (a, b) => $"({a} - {b})"},
-            {Op.E, (a, b) => $"({a} ^ {b})"},
-            {Op.C, (a, b) => $"({a} | {b})"}
-        };
-
-        public static int CountOps(string s)
-        {
-            return s.Count(c => "*/+-^|".Contains(c));
-        }
-
-        public LookupDict LookupDictWithGroupings(bool parallel = false)
+        /// <summary>
+        /// Populate a LookupDict with all adjacent groups of numbers, e.g.: 1, 12, 123, 1234... 89, 9 
+        /// </summary>
+        LookupDict LookupDictWithGroupings()
         {
             var nums = Enumerable.Range(1, 9).ToArray();
             var compDict = new LookupDict();
-            if (parallel)
+
+            // Parallel seemed slower here, hmmm...
+            foreach (var min in nums)
             {
-                // this one is slower!  probably waste a lot of time waiting for locks...
-                Parallel.ForEach(nums, min =>
+                foreach (var max in nums)
                 {
-                    Parallel.ForEach(nums, max =>
+                    if (max >= min)
                     {
-                        if (max >= min)
-                        {
-                            var valAsString = string.Join("", Enumerable.Range(min, max - min + 1).Select(x => $"{x}"));
-                            compDict[(Convert.ToInt32(valAsString), min, max)] = $"{valAsString}";
-                        }
-                    });
-                });
-            }
-            else
-            {
-                foreach (var min in nums)
-                {
-                    foreach (var max in nums)
-                    {
-                        if (max >= min)
-                        {
-                            var valAsString = string.Join("", Enumerable.Range(min, max - min + 1).Select(x => $"{x}"));
-                            compDict[(Convert.ToInt32(valAsString), min, max)] = $"{valAsString}";
-                        }
+                        var valAsString = string.Join("", Enumerable.Range(min, max - min + 1).Select(x => $"{x}"));
+                        compDict[(Convert.ToInt32(valAsString), min, max)] = $"{valAsString}";
                     }
                 }
             }
@@ -92,86 +49,80 @@ namespace OperatorJumble
             return compDict;
         }
 
-        public void MergeDicts(LookupDict compDict, LookupDict nextGen)
+        /// <summary>
+        /// Add to the index all the elements in the initial LookupDict
+        /// </summary>
+        void PopulateIndex(LookupDict compDict, LookupIndex index)
         {
-            foreach (var kv in nextGen)
+            foreach (var kv in compDict)
             {
-                if (!compDict.ContainsKey(kv.Key)
-                    && kv.Key.val != Int32.MaxValue)
-                {
-                    compDict.TryAdd(kv.Key, kv.Value);
-                }
+                index[kv.Key.max - kv.Key.min + 1].Add(kv.Key);
             }
         }
 
-        public LookupDict BuildCompDict(int levels, bool parallel = false)
+        /// <summary>
+        /// Build a LookupDict for the number of levels specified.  The number of levels is the number of values in
+        /// an expression.  The resulting dictionary will include all the levels less than the specified level as well.
+        /// </summary>
+        public LookupDict BuildCompDict(int levels)
         {
             var ops = new List<Op> { Op.A, Op.S, Op.M, Op.D, Op.E, Op.C };
-            var compDict = LookupDictWithGroupings(parallel);
-            int level = 2;
-            while (level++ <= levels)
-            {
-                var nextGen = new LookupDict();
-                if (parallel)
-                {
-                    Parallel.ForEach(compDict, kv1 =>
-                    {
-                        Parallel.ForEach(compDict, kv2 =>
-                        {
-                            if (kv2.Key.min == kv1.Key.max + 1)
-                            {
-                                Parallel.ForEach(ops, op =>
-                                {
-                                    try
-                                    {
-                                        var v = evalDict[op](kv1.Key.val, kv2.Key.val);
-                                        nextGen[(v, kv1.Key.min, kv2.Key.max)] = exprDict[op](kv1.Value, kv2.Value);
-                                    }
-                                    catch
-                                    {
-                                    }
-                                });
-                            }
-                        });
-                    });
-                }
-                else
-                {
-                    foreach (var kv1 in compDict)
-                    {
-                        foreach (var kv2 in compDict)
-                        {
-                            if (kv2.Key.min == kv1.Key.max + 1)
-                            {
-                                foreach (var op in ops)
-                                {
-                                    try
-                                    {
-                                        var v = evalDict[op](kv1.Key.val, kv2.Key.val);
-                                        nextGen[(v, kv1.Key.min, kv2.Key.max)] =
-                                            exprDict[op](kv1.Value, kv2.Value);
-                                    }
-                                    catch
-                                    {
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
 
-                MergeDicts(compDict, nextGen);
+            var index = GenerateIndex();
+            var compDict = LookupDictWithGroupings();
+            PopulateIndex(compDict, index);
+
+            int level = 1;
+            while (level++ < levels)
+            {
+                Parallel.ForEach(index, kv1 =>
+               {
+                   Parallel.ForEach(index, kv2 =>
+                   {
+                       if (kv1.Key + kv2.Key == level)
+                       {
+                           Parallel.ForEach(index[kv1.Key], i1 =>
+                           {
+                               Parallel.ForEach(index[kv2.Key], i2 =>
+                               {
+                                   if (i1.max + 1 == i2.min)
+                                   {
+                                       Parallel.ForEach(ops, op =>
+                                       {
+                                           try
+                                           {
+                                               var v = evalDict[op](i1.val, i2.val);
+                                               if (v != Int32.MaxValue)
+                                               {
+                                                   var expr = exprDict[op](compDict[(i1.val, i1.min, i1.max)], compDict[(i2.val, i2.min, i2.max)]);
+                                                   if (!compDict.ContainsKey((v, i1.min, i2.max)))
+                                                   {
+                                                       compDict[((v, i1.min, i2.max))] = expr;
+                                                       index[level].Add((v, i1.min, i2.max));
+                                                   }
+                                                   else if (CountOps(compDict[(v, i1.min, i2.max)]) > CountOps(expr))
+                                                   {
+                                                       compDict[((v, i1.min, i2.max))] = expr;
+                                                   }
+                                               }
+                                           }
+                                           catch { }
+                                       });
+                                   }
+                               });
+                           });
+                       }
+                   });
+               });
             }
 
             return compDict;
         }
-
-        
 
         public string Run(int n)
         {
             var level = 9;
-            var compDict = BuildCompDictWithIndex(level, false);
+            var compDict = BuildCompDict(level);
             using (var f = new StreamWriter($"C:\\Users\\waldr\\Documents\\opjum\\seqIndexOut{level}.csv"))
             {
                 foreach (var kv in compDict.Where(x => (x.Key.max - x.Key.min + 1 == level) && (x.Key.val > 0)).OrderBy(x => x.Key.val))
