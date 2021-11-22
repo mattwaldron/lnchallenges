b732634041e464cf360fd5675a13f90691ab41de
Phillip LaBar <Phillip.LaBar@Livanova.com>
2021-11-22 15:04:12 -0600

    Added solution tester

--- CodingChallengeFramework/CodingChallengeFramework/ICrackerBarrel.cs
+++ CodingChallengeFramework/CodingChallengeFramework/ICrackerBarrel.cs
@@ -23,6 +23,10 @@ namespace CodingChallengeFramework
 
         public static string ComputeSolution(List<(ushort jumpStartSpace, ushort jumpEndSpace)> result, short baseSize)
         {
+            List<Jump> fullJumpList = new List<Jump>();
+            List<Pin> fullPinList = CreatePinsAndJumpTable(baseSize, fullJumpList);
+            InitPinJumps(fullPinList, fullJumpList);
+
             if(result.Count == 0)
             {
                 return "No Solution";
@@ -33,11 +37,25 @@ namespace CodingChallengeFramework
             }
             else if (result.Count == OnePinLeftJumpCount(baseSize) && result[0].jumpEndSpace == result[result.Count-1].jumpEndSpace)
             {
-                return "1 pin left in starting hole";
+                if(VerifyJumps(result,fullPinList))
+                {
+                    return "Prime solution found - 1 pin left in starting hole";
+                }
+                else
+                {
+                    return "Invalid solution - jumps not valid";
+                }                
             }
             else
             {
-                return "1 pin left not in starting hole";
+                if(VerifyJumps(result,fullPinList))
+                {
+                    return "Non-Prime Solution found - 1 pin left not in starting hole";
+                }
+                else
+                {
+                    return "Invalid solution - jumps not valid";
+                }               
             }          
         }
 
@@ -78,7 +96,131 @@ namespace CodingChallengeFramework
                     answer = $" !!! Threw exception with message: {ex.Message}";
                 }
                 Console.WriteLine($"{q.GetType().Name} (in {sw.ElapsedMilliseconds} ms) << {answer}");
+                Console.ReadKey();
             }           
         }
+
+        public static List<Pin> CreatePinsAndJumpTable(int baseSize, List<Jump> jumps)
+        {           
+            List<Pin> retPinList = new List<Pin>();
+            int PinID = 1;           
+            for (int tier = 1; tier <= baseSize; tier++)
+            {  
+                for (int tierPos = 1; tierPos <= tier; tierPos++)
+                {
+                    //Set Pin
+                    retPinList.Add(new Pin(PinID));                 
+
+                    //Set Jumps for each Pin
+                    if (tier > 2)
+                    {
+                        if (tierPos < (tier - 1))
+                        {
+                            // add SW<->NE jump pins
+                            int SW_NEMiddlePinID = PinID - (tier - 1);
+                            jumps.Add(new Jump(PinID, SW_NEMiddlePinID, SW_NEMiddlePinID - (tier - 2)));
+                            jumps.Add(new Jump(SW_NEMiddlePinID - (tier - 2), SW_NEMiddlePinID, PinID));
+                            // W<->E jump pins
+                            jumps.Add(new Jump(PinID, PinID + 1, PinID + 2));
+                            jumps.Add(new Jump(PinID + 2, PinID + 1, PinID));
+                        }
+
+                        if (tierPos > 2)
+                        {
+                            // add NW<->SE jump pins
+                            int NW_SEMiddlePin = PinID - tier;
+                            jumps.Add(new Jump(PinID, NW_SEMiddlePin, NW_SEMiddlePin - (tier - 1)));
+                            jumps.Add(new Jump(NW_SEMiddlePin - (tier - 1), NW_SEMiddlePin, PinID));
+                        }
+                    }
+
+                    PinID++;
+                }
+            }
+
+            return retPinList;
+        }
+        
+        public static void InitPinJumps(List<Pin> pins, List<Jump> jumpList)
+        {
+            foreach (Pin p in pins)
+            {
+                foreach (Jump j in jumpList)
+                {
+                    if (j.startPin == p.ID)
+                    {
+                        p.Jumps.Add(j);
+                    }
+                }
+            }
+        }
+
+        public static bool VerifyJumps(List<(ushort jumpStartSpace, ushort jumpEndSpace)> result, List<Pin> pins)
+        {
+            //Set open pin
+            pins[result[0].jumpEndSpace - 1].Epmty = true;
+
+            foreach((ushort jumpStartSpace, ushort jumpEndSpace) jump in result)
+            {
+                if(!PerformJump(jump,pins))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool PerformJump((ushort jumpStartSpace, ushort jumpEndSpace) jump, List<Pin> pins)
+        {
+            Pin p = pins[jump.jumpStartSpace - 1];
+
+            // Make sure it has valid jump
+            foreach(Jump j in p.Jumps)
+            {
+                // Find jump
+                if(j.startPin == jump.jumpStartSpace && j.endPin == jump.jumpEndSpace)
+                {
+                    // validate jump
+                    if(!pins[j.startPin - 1].Epmty && !pins[j.middlePin - 1] .Epmty && pins[j.endPin - 1].Epmty)
+                    {
+                        // Do jump
+                        pins[j.startPin - 1].Epmty = true;
+                        pins[j.middlePin - 1].Epmty =  true;
+                        pins[j.endPin - 1].Epmty = false;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public class Pin
+        {
+            public int ID;
+            public bool Epmty;
+            public List<Jump> Jumps = new List<Jump>();     
+
+            public Pin(int pinID)
+            {
+                ID = pinID;
+                Epmty = false;
+            }
+        }
+
+        public class Jump
+        {
+            public int startPin;
+            public int middlePin;
+            public int endPin;      
+
+            public Jump(int start, int middle, int end)
+            {
+                startPin = start;
+                middlePin = middle;
+                endPin = end;
+            }
+        }   
     }
 }
--- CodingChallengeFramework/CrackerBarrel/BaseSizeFiveExample.cs
+++ CodingChallengeFramework/CrackerBarrel/BaseSizeFiveExample.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,11 @@ namespace CrackerBarrel
 {
     public class BaseSizeFiveExample : ICrackerBarrel
     {
+
         public List<(ushort jumpStartSpace, ushort jumpEndSpace)> Run(byte baseSize)
         {
-            // Your solution here
-
-            // Example return for a base of 5
-
-            List<(ushort index, ushort length)> retVector = new List<(ushort jumpStartSpace, ushort jumpEndSpace)>();
-
-            return new List<(ushort jumpStartSpace, ushort jumpEndSpace)> { (13, 6), (15, 13), (6, 15), (7, 9), (12, 14), (15, 13), (13, 6), (2, 7), (11, 4), (3, 10), (4, 6), (10, 3), (1, 6) };
+            return new List<(ushort jumpStartSpace, ushort jumpEndSpace)> { (13, 6), (15, 13), (6, 15), (7, 9), (12, 14), (15, 13), (13, 6), (2, 7), (11, 4), (3, 10), (4, 6), (10, 3), (1, 6) };         
         }
     }
+
 }
